0146270a62f6a8b921c1ff28ea0fec8b26f97551
vaids12 <vaidasvaupsas@gmail.com>
2021-12-27 21:33:39 +0200

    update

--- jQuery practice/dist/js/script.js
+++ jQuery practice/dist/js/script.js
@@ -45,13 +45,73 @@ $(document).ready(function () {
 
 //Form control
 
-$(document).ready(function(){
-   const data = [];
-   $("#submit").click(function(e){
-       e.preventDefault();
-       let firstName = $("input[name=firstName]").val();
-       let email= $("input[name=email]").val();
-       console.log( firstName +" , "+ email );
-   })
+$(document).ready(function () {
+    const data = [];
+    //duomenu ivedimas
+    $("#submit").click(function (e) {
+        e.preventDefault();
+        
+        let firstName = $("input[name=firstName]").val();
+        let email = $("input[name=email]").val();
+        if(!firstName || !email){
+            return alert("Suveskite duomenis!")
+        }
+        //console.log( firstName +" , "+ email );
+        let user = firstName + ", " + email;
+        //console.log(user);
+        data.push(user); // iraso kintamaji i masyva    
+        $("input[name=firstName]").val("");
+        $("input[name=email]").val("");
+        //    console.log(data);
+        $("#list").empty();
+            for (let i = 0; i < data.length; i++) {
+                $("#list").append("<li>" + data[i] + "</li>");
+            }
+        
+        // $("#list").append("<li>" + data[data.length - 1] + "</li>");
+    });
+    // duomenu slepimas/rodymas
+    $("#clearList").click(function () {
+        $("#list").empty();
+    });
+    $("#showArray").click(function () {
+        if(data.length==0){
+            alert("Duomenu nėra!")
+        }
+        $("#list").empty();
+        for (let i = 0; i < data.length; i++) {
+            $("#list").append("<li>" + data[i] + "</li>");
+        }
+    })
+
+    //duomenu istrynimas
+
+    $("#clearArray").click(function(){
+        alert("Duomenys bus ištrinti!")
+        data.splice(0,data.length);
+        $("#list").empty();
+    })
 
 });
+
+//dropdown
+
+$(document).ready(function(){
+$("#menuMain").hover(function(){
+    $("#main").toggleClass("show");
+
+});
+$("#aboutMenu").hover(function(){
+    $("#about").toggleClass("show");
+
+});
+$("#contactsMenu").hover(function(){
+    $("#contacts").toggleClass("show");
+
+});
+$("#formMenu").hover(function(){
+    $("#dataForm").toggleClass("show");
+
+});
+});
+
